1a7351a931f7dccf86034f4532159994fb5b54d7
adriton <adrtler@gmail.com>
2020-10-01 21:48:33 +0200

    Parse del body json

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,16 +2,23 @@
 const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
+  const { name, email, password } = req.body;
   res.json({
     ok: true,
     msg: 'register',
+    name,
+    email,
+    password,
   });
 };
 
 const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
   res.json({
     ok: true,
     msg: 'login',
+    email,
+    password,
   });
 };
 
