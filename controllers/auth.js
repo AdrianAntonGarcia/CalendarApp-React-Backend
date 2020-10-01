6dbc1ab478f675b2192c9bd08992bb339a175d74
adriton <adrtler@gmail.com>
2020-10-01 22:02:58 +0200

    Express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,19 @@
 // Para recuperar el tipado en el visual studio
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
-  res.json({
+
+  // Manejo de errores
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: 'false',
+      errors: errors.mapped(),
+    });
+  }
+  res.status(201).json({
     ok: true,
     msg: 'register',
     name,
@@ -14,7 +24,14 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
   const { email, password } = req.body;
-  res.json({
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: 'false',
+      errors: errors.mapped(),
+    });
+  }
+  res.status(200).json({
     ok: true,
     msg: 'login',
     email,
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@
  */
 
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 
 const {
@@ -12,9 +13,31 @@ const {
   revalidarToken,
 } = require('../controllers/auth');
 
-router.post('/new', crearUsuario);
+/**
+ * Colección de middlewares como segundo parámetro
+ */
+router.post(
+  '/new',
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({
+      min: 6,
+    }),
+  ],
+  crearUsuario
+);
 
-router.post('/', loginUsuario);
+router.post(
+  '/',
+  [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({
+      min: 6,
+    }),
+  ],
+  loginUsuario
+);
 
 router.get('/renew', revalidarToken);
 
