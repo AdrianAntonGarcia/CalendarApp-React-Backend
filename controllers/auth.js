a04206f09b81c782c210c62e5cc356d37d811cbd
adriton <adrtler@gmail.com>
2020-10-02 19:15:10 +0200

    Generación json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body;
@@ -22,10 +23,16 @@ const crearUsuario = async (req, res = response) => {
     usuario.password = bcrypt.hashSync(password, salt);
 
     await usuario.save();
+
+    // Generar JWT
+
+    const token = await generarJWT(usuario.id, usuario.name);
+
     res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
   } catch (error) {
     console.log(error);
@@ -57,12 +64,14 @@ const loginUsuario = async (req, res = response) => {
       });
     }
     // Generar nuestro JWT
+    const token = await generarJWT(usuario.id, usuario.name);
 
     res.status(200).json({
       ok: true,
       msg: 'login',
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
   } catch (error) {
     console.log(error);
