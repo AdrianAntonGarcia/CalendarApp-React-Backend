ec7e7430274f218f079937ae2213680db001e6e0
adriton <adrtler@gmail.com>
2020-10-03 19:30:32 +0200

    Servicio de eliminación de eventos

--- controllers/events.js
+++ controllers/events.js
@@ -74,10 +74,37 @@ const actualizarEvento = async (req, res = response) => {
 };
 
 const eliminarEvento = async (req, res = response) => {
-  res.status(201).json({
-    ok: true,
-    msg: 'eliminar evento',
-  });
+  try {
+    const eventoId = req.params.id;
+    const evento = await Evento.findById(eventoId);
+    const uid = req.uid;
+    if (!evento) {
+      res.status(404).json({
+        ok: false,
+        msg: 'Evento no existe por ese id',
+      });
+    }
+    /*Hay que comprobar que la persona que está consultando 
+    sea la misma que la que creo el evento*/
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegio de eliminar este evento',
+      });
+    }
+
+    const eventoBorrado = await Evento.findByIdAndDelete(eventoId);
+    res.status(201).json({
+      ok: true,
+      eventoBorrado
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador',
+    });
+  }
 };
 
 module.exports = { getEventos, crearEvento, actualizarEvento, eliminarEvento };
