61c29882e9789a605581db7eeece9202be0b1b6e
adriton <adrtler@gmail.com>
2020-10-03 18:37:32 +0200

    Validación fechas con moment y express validator

--- routes/events.js
+++ routes/events.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const {
   getEventos,
@@ -16,7 +19,19 @@ router.use(validarJWT);
 router.get('/', getEventos);
 
 // Crear un nuevo evento
-router.post('/', crearEvento);
+
+/*Express validator no tiene para validar fechas, por lo que haremos 
+una manual. Con el custom mandamos una función personalizada de validación */
+router.post(
+  '/',
+  [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+    validarCampos,
+  ],
+  crearEvento
+);
 
 // Actualizar Evento
 router.put('/:id', actualizarEvento);
