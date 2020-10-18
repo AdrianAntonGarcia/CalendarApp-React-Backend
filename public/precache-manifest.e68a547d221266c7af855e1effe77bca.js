8ade7f0bb3f4b63d7791d035e0690318e5ef4642
adriton <adrtler@gmail.com>
2020-10-18 12:12:35 +0200

    v1 CalendarApp

---
+++ public/precache-manifest.e68a547d221266c7af855e1effe77bca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ff3574e25e413904295e16a0bf98c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7609ce0746b9fa9c5b71",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "4aa8c25abac094700bb9",
+    "url": "/static/css/main.34819481.chunk.css"
+  },
+  {
+    "revision": "7609ce0746b9fa9c5b71",
+    "url": "/static/js/2.447dd372.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.447dd372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa8c25abac094700bb9",
+    "url": "/static/js/main.cc005625.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
